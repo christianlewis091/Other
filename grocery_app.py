9486990f0a4d18d69efbb1454bb7f63a1e4fb93d
christianlewis091 <christian.lewis091@gmail.com>
2022-08-21 19:58:48 +1200

    changed grocery app to dropdown menu

--- grocery_app.py
+++ grocery_app.py
@@ -6,20 +6,16 @@ import numpy as np
 
 # TODO fix the format of the window, the user interface essentially is hard to read/ugly at the moment, but it works!
 df = pd.read_excel(r'C:\Users\lewis\venv\python310\python-masterclass-remaster-shared\finances\recipebook.xlsx')
-# print(df)
 recipes = (np.unique(df['Recipe Title']))  # grab each unique recipe from the recipe book and print it so the
+recipes = pd.DataFrame({"Recipe": recipes})
+recipes = recipes.sort_values(by='Recipe')
 
 root = Tk()
-root['bg'] = '#ffbf00'
-
-
-# df = pd.read_excel(r'C:\Users\lewis\venv\python310\python-masterclass-remaster-shared\finances\recipebook.xlsx')
-# recipes = (np.unique(df['Recipe Title']))  # grab each unique recipe from the recipe book and print it so the
-# user can see what their options are. This will be superceded by a
-# dropdown menu when I get the GUI running.
-
-hello_message = Label(root, text="Hello! Welcome to the grocery list generator")
-# hello_message.grid(row=0, column=0)
+# root['bg'] = '#ffbf00'
+# root.geometry("700x500")
+hello_message = Label(root, text=
+"Hello! Welcome to the grocery list generator")
+hello_message.grid(row=0, rowspan=2, column=0, columnspan=5)
 
 mon = Label(root, text="M")
 tues = Label(root, text="Tu")
@@ -32,112 +28,115 @@ breakfast = Label(root, text="Breakfast")
 lunch = Label(root, text="Lunch")
 dinner = Label(root, text="Dinner")
 
-mon.grid(row=2, column=1)
-tues.grid(row=2, column=2)
-wed.grid(row=2, column=3)
-thurs.grid(row=2, column=4)
-fri.grid(row=2, column=5)
-sat.grid(row=2, column=6)
-sun.grid(row=2, column=7)
-breakfast.grid(row=3, column=0)
-lunch.grid(row=4, column=0)
-dinner.grid(row=5, column=0)
+mon.grid(row=1, column=6)
+tues.grid(row=2, column=6)
+wed.grid(row=3, column=6)
+thurs.grid(row=4, column=6)
+fri.grid(row=5, column=6)
+sat.grid(row=6, column=6)
+sun.grid(row=7, column=6)
+breakfast.grid(row=0, column=7)
+lunch.grid(row=0, column=8)
+dinner.grid(row=0, column=9)
 
+values = ['all'] + list(recipes['Recipe'].unique())
 e1 = StringVar()
-e1_entry = ttk.Entry(root, width=30, textvariable=e1)
-e1_entry.grid(row=3, column=1)
+e1_entry = ttk.OptionMenu(root, e1, *values)
+e1_entry.grid(row=1, column=7)
 
 e2 = StringVar()
-e2_entry = ttk.Entry(root, width=30, textvariable=e2)
-e2_entry.grid(row=3, column=2)
+e2_entry = ttk.OptionMenu(root, e2, *values)
+e2_entry.grid(row=1, column=8)
 
 e3 = StringVar()
-e3_entry = ttk.Entry(root, width=30, textvariable=e3)
-e3_entry.grid(row=3, column=3)
+e3_entry = ttk.OptionMenu(root, e3, *values)
+e3_entry.grid(row=1, column=9)
 
 e4 = StringVar()
-e4_entry = ttk.Entry(root, width=30, textvariable=e4)
-e4_entry.grid(row=3, column=4)
+e4_entry = ttk.OptionMenu(root, e4, *values)
+e4_entry.grid(row=2, column=7)
 
 e5 = StringVar()
-e5_entry = ttk.Entry(root, width=30, textvariable=e5)
-e5_entry.grid(row=3, column=5)
+e5_entry = ttk.OptionMenu(root, e5, *values)
+e5_entry.grid(row=2, column=8)
 
 e6 = StringVar()
-e6_entry = ttk.Entry(root, width=30, textvariable=e6)
-e6_entry.grid(row=3, column=6)
+e6_entry = ttk.OptionMenu(root, e6, *values)
+e6_entry.grid(row=2, column=9)
 
 e7 = StringVar()
-e7_entry = ttk.Entry(root, width=30, textvariable=e7)
+e7_entry = ttk.OptionMenu(root, e7, *values)
 e7_entry.grid(row=3, column=7)
 
 e8 = StringVar()
-e8_entry = ttk.Entry(root, width=30, textvariable=e8)
-e8_entry.grid(row=4, column=1)
+e8_entry = ttk.OptionMenu(root, e8, *values)
+e8_entry.grid(row=3, column=8)
 
 e9 = StringVar()
-e9_entry = ttk.Entry(root, width=30, textvariable=e9)
-e9_entry.grid(row=4, column=2)
+e9_entry = ttk.OptionMenu(root, e9, *values)
+e9_entry.grid(row=3, column=9)
 
 e10 = StringVar()
-e10_entry = ttk.Entry(root, width=30, textvariable=e10)
-e10_entry.grid(row=4, column=3)
+e10_entry = ttk.OptionMenu(root, e10, *values)
+e10_entry.grid(row=4, column=7)
 
 e11 = StringVar()
-e11_entry = ttk.Entry(root, width=30, textvariable=e11)
-e11_entry.grid(row=4, column=4)
+e11_entry = ttk.OptionMenu(root, e11, *values)
+e11_entry.grid(row=4, column=8)
 
 e12 = StringVar()
-e12_entry = ttk.Entry(root, width=30, textvariable=e12)
-e12_entry.grid(row=4, column=5)
+e12_entry = ttk.OptionMenu(root, e12, *values)
+e12_entry.grid(row=4, column=9)
 
 e13 = StringVar()
-e13_entry = ttk.Entry(root, width=30, textvariable=e13)
-e13_entry.grid(row=4, column=6)
+e13_entry = ttk.OptionMenu(root, e13, *values)
+e13_entry.grid(row=5, column=7)
 
 e14 = StringVar()
-e14_entry = ttk.Entry(root, width=30, textvariable=e14)
-e14_entry.grid(row=4, column=7)
+e14_entry = ttk.OptionMenu(root, e14, *values)
+e14_entry.grid(row=5, column=8)
 
 e15 = StringVar()
-e15_entry = ttk.Entry(root, width=30, textvariable=e15)
-e15_entry.grid(row=5, column=1)
+e15_entry = ttk.OptionMenu(root, e15, *values)
+e15_entry.grid(row=5, column=9)
 
 e16 = StringVar()
-e16_entry = ttk.Entry(root, width=30, textvariable=e16)
-e16_entry.grid(row=5, column=2)
+e16_entry = ttk.OptionMenu(root, e16, *values)
+e16_entry.grid(row=6, column=7)
 
 e17 = StringVar()
-e17_entry = ttk.Entry(root, width=30, textvariable=e17)
-e17_entry.grid(row=5, column=3)
+e17_entry = ttk.OptionMenu(root, e17, *values)
+e17_entry.grid(row=6, column=8)
 
 e18 = StringVar()
-e18_entry = ttk.Entry(root, width=30, textvariable=e18)
-e18_entry.grid(row=5, column=4)
+e18_entry = ttk.OptionMenu(root, e18, *values)
+e18_entry.grid(row=6, column=9)
 
 e19 = StringVar()
-e19_entry = ttk.Entry(root, width=30, textvariable=e19)
-e19_entry.grid(row=5, column=5)
+e19_entry = ttk.OptionMenu(root, e19, *values)
+e19_entry.grid(row=7, column=7)
 
 e20 = StringVar()
-e20_entry = ttk.Entry(root, width=30, textvariable=e20)
-e20_entry.grid(row=5, column=6)
+e20_entry = ttk.OptionMenu(root, e20, *values)
+e20_entry.grid(row=7, column=8)
 
 e21 = StringVar()
-e21_entry = ttk.Entry(root, width=30, textvariable=e21)
-e21_entry.grid(row=5, column=7)
+e21_entry = ttk.OptionMenu(root, e21, *values)
+e21_entry.grid(row=7, column=9)
 
 
 def myClick():
-    myLabel = Label(root, text=recipes)
-    myLabel.grid(row=2, column=0)
+    top = Toplevel()
+    myLabel = Label(top, text=recipes, justify=LEFT).pack()
+
 
 myButton = Button(root, text="Click here to see available recipes", command=myClick, fg='blue')
-myButton.grid(row=1, column=0)
+myButton.grid(row=2, rowspan=5, column=0, columnspan=5)
+
 
 def executeList():
     myLabel2 = Label(root, text="List has been created...I think")
-    myLabel2.grid(row=8, column=0)
+    myLabel2.grid(row=10, column=6, columnspan=5)
     value1 = str(e1.get())
     value2 = str(e2.get())
     value3 = str(e3.get())
@@ -231,47 +230,49 @@ def executeList():
         if value21 == item:  # if the input is equal to a specific item,
             df_new = df.loc[(df['Recipe Title'] == item)]  # take the first input from the user, and find where in the
             x = pd.concat([x, df_new])  # then add it to the DataFrame that we're building for the grocery list
-    if len(x) != 0:
-        x['Duplicate_search'] = x.duplicated(subset='Ingredient', keep=False)  # This function identifies duplicates, by adding a new column and setting all dups to True.
-        # by setting keep = False, all dups are True
 
-        nondups = x.loc[(x['Duplicate_search'] == False)]                   # locate a list of all NON duplicates, and save for later use
-        dups = x.loc[(x['Duplicate_search'] == True)]                       # locate a lost of ALL duplicates, to work with now
-        dups = np.unique(dups['Ingredient'])                                # Create a list of all the unique ingredients that are duplicated
-        for item in dups:                                                   # iterate through the list of duplicates
-            current_ingredient = x.loc[(x['Ingredient'] == item)]           # find all those items in the whole list
-            if len(np.unique(current_ingredient['Unit of Measure'])) == 1:  # if there is only one type of unit measure,
-                sum = np.sum(current_ingredient['Quantity'])                # add the quantities together
-                current_ingredient['Quantity'] = sum                        # change the "quantity" to the sum:
-                # currently, there will be dups on the main sheet, but they'll all have the total quantity, and I'll just drop the dups later
+    x['Duplicate_search'] = x.duplicated(subset='Ingredient',
+                                         keep=False)  # This function identifies duplicates, by adding a new column and setting all dups to True.
+    # by setting keep = False, all dups are True
+    x = x.sort_values(by='Ingredient')
+    x.to_excel('step1.xlsx')
 
-                recipes = np.unique(current_ingredient['Recipe Title'])     # this next block of code will string all the recipes together that call for the duplicate items
-                strings = ""                                                # so that the user can see at a glance when looking at the grocery list
-                for k in range(0, len(recipes)):
-                    name = recipes[k]
-                    strings = strings + str(name) + str("\u007e")
-                current_ingredient['Recipe Title'] = strings
-                nondups = pd.concat([nondups, current_ingredient])          # concat this duplicated ingredient onto the list of nondups
+    nondups = x.loc[(x['Duplicate_search'] == False)]  # locate a list of all NON duplicates, and save for later use
+    dups = x.loc[(x['Duplicate_search'] == True)]  # locate a lost of ALL duplicates, to work with now
+    dups = np.unique(dups['Ingredient'])  # Create a list of all the unique ingredients that are duplicated
+    print(dups)
+    for item in dups:  # iterate through the list of duplicates
+        current_ingredient = x.loc[(x['Ingredient'] == item)]  # search the ENTIRE grocery list for all instances of the iterable item
 
-            elif len(np.unique(current_ingredient[
-                                   'Unit of Measure'])) != 1:               # if there are duplicate ingredients but they have different units of measure,
-                nondups = pd.concat([nondups, current_ingredient])          # make sure that's back on the main list as well.
+        if len(np.unique(current_ingredient['Unit of Measure'])) == 1:  # if there is only one type of unit measure,
+            sum_of_ingredients = np.sum(current_ingredient['Quantity'])  # add the quantities together
+            current_ingredient['Quantity'] = sum_of_ingredients  # change the "quantity" to the sum:
+            #       # currently, there will be dups on the main sheet, but they'll all have the total quantity, and I'll just drop the dups later
+            #
+            for_tildas = np.unique(current_ingredient['Recipe Title'])     # this next block of code will string all the recipes together that call for the duplicate items
+            strings = ""                                                # so that the user can see at a glance when looking at the grocery list
+            for k in range(0, len(for_tildas)):
+                name = for_tildas[k]
+                strings = strings + str(name) + str("\u007e")
+            current_ingredient['Recipe Title'] = strings
+            nondups = pd.concat([nondups, current_ingredient])          # concat this duplicated ingredient onto the list of nondups
+        #
+        elif len(np.unique(current_ingredient['Unit of Measure'])) != 1:  # if theres duplicates that aren't the same unit of measure
+            nondups = pd.concat([nondups, current_ingredient])          # make sure that's back on the main list as well.
 
-        """
-        Now, at this point, we're just trying to get a grocery list, so I'm going to drop the recipe title, and then
-        drop the duplicates.
-        """
-        nondups = nondups[['Ingredient', 'Quantity', 'Unit of Measure', 'Recipe Title']].reset_index(drop=True)
-        nondups['Dups to Drop'] = nondups.duplicated()
-        final_list = nondups.loc[(nondups[
-                                      'Dups to Drop'] == False)]  # grab only the items that the above function sets to false. (All duplicates are set to true, so keep all the false ones)
-        final_list.to_excel('recipetest.xlsx')
-
-
-
+    nondups.to_excel('step2.xlsx')
+    """
+    Now, at this point, we're just trying to get a grocery list, so I'm going to drop the recipe title, and then
+    drop the duplicates.
+    """
+    nondups = nondups[['Ingredient', 'Quantity', 'Unit of Measure', 'Recipe Title']].reset_index(drop=True)
+    nondups['Dups to Drop'] = nondups.duplicated()
+    final_list = nondups.loc[(nondups[
+                                  'Dups to Drop'] == False)]  # grab only the items that the above function sets to false. (All duplicates are set to true, so keep all the false ones)
+    final_list.to_excel('recipetest.xlsx')
 
 
 myButton = Button(root, text="Execute list creation", command=executeList, fg='blue')
-myButton.grid(row=7, column=0)
+myButton.grid(row=9, column=6, columnspan=5)
 
 root.mainloop()
--- testingspace.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from finance_functions import long_date_to_decimal_date
-import pandas as pd
-
-df = pd.read_excel(r'C:\Users\lewis\venv\python310\python-masterclass-remaster-shared\finances\brazil.xlsx')
-x = df['Brazil']
-x = long_date_to_decimal_date(x)
-print(x)
-
