cbae7c7e4281af3e0cef571592ee682d0fd08e17
christianlewis091 <christian.lewis091@gmail.com>
2023-06-01 15:46:37 +1200

    editing trajectory creator based on conversation with JT

--- hysplit_make_plots.py
+++ hysplit_make_plots.py
@@ -5,6 +5,10 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 import subprocess
 import pandas as pd
+import shapely
+import warnings
+from shapely.errors import ShapelyDeprecationWarning
+warnings.filterwarnings("ignore", category=ShapelyDeprecationWarning)
 from os import listdir
 from os.path import isfile, join
 
@@ -12,42 +16,250 @@ from os.path import isfile, join
 6/1/23: version 2 of this file that prepares the Hysplit output for plotting: 
 Needs updating to do trajectories for everty site. Old version commented out below
 """
-
 # We're still going to loop through each site using the codenames listed in the previous scripts as well
-df = pd.read_excel(r'C:/easy_access.xlsx')
-codenames = df['Codename']
+easy_access = pd.read_excel(r'C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/easy_access2.xlsx')
+codenames = easy_access['Codename']
 
-# # Read in the sheet that was made from the prvious script (hysplit_prepare_output.py)
-# means_dataframe = pd.read_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/means_dataframe.xlsx')
+# Read in the sheet that was made from the prvious script (hysplit_prepare_output.py)
+means_dataframe = pd.read_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/means_dataframe.xlsx')
+
+# Read in the sheet that was made from the prvious script (hysplit_prepare_output.py)
+points = pd.read_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/points.xlsx')
+
+
+# Loop through the codenames
+for i in range(0, len(codenames)):
+
+    # here are the POINTS that we'll be plotting
+    site_points = points.loc[points['location'] == codenames[i]]
+
+    # here are the means that we'll be plotting:
+    site_means = means_dataframe.loc[means_dataframe['Codename'] == codenames[i]]
+
+    # define map boundaries
+    mapcorners = [0, -70, 360, -20]
+    maxlat = mapcorners[3]
+    minlat = mapcorners[1]
+    maxlon = mapcorners[2]
+    minlon = mapcorners[0]
+
+    # Initialize the Figure
+    fig = plt.figure(figsize=(4, 4))
+    gs = gridspec.GridSpec(6, 4)
+    gs.update(wspace=.25, hspace=0.15)
+
+    # Initialize first subplot
+    xtr_subsplot = fig.add_subplot(gs[0:4, 0:4])
+
+    # build the map
+    map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=minlon, urcrnrlon=maxlon, resolution='l', lon_0=-180)
+    map.drawmapboundary(fill_color='lightgrey')
+    map.fillcontinents(color='darkgrey')
+    map.drawcoastlines(linewidth=0.1)
+
+    y, x = map(site_points['y'], site_points['x'])
+    y_mean, x_mean = (site_means['y'], site_means['x'])
+    map.scatter(x, y, marker='x', color='black', alpha=0.01, label=f'{codenames[i]}')
+    map.scatter(x_mean, y_mean, marker='o', color='red', alpha=0.01)
+    map.drawparallels(np.arange(-90, 90, 10), labels=[True, False, False, False], linewidth=0.5)
+    map.drawmeridians(np.arange(-180, 180, 10), labels=[1, 1, 0, 1], linewidth=0.5)
+
+    xtr_subsplot = fig.add_subplot(gs[4:6, 0:4])
+    plt.scatter(site_means['timestep'], site_means['z'])
+    plt.show()
+
+
+
 #
-# for i in range(0, len(codenames)):
-#     # locate based on the codename
-#     this_site = means_dataframe.loc[means_dataframe['Codename'] == codenames[i]]
+# for i in range(0, len(groups)):
 #
+#     # Initialize the Figure
 #     fig = plt.figure(figsize=(4, 4))
 #     gs = gridspec.GridSpec(6, 4)
 #     gs.update(wspace=.25, hspace=0.15)
+#     standard_pm = None
 #
+#     # Initialize first subplot
+#     xtr_subsplot = fig.add_subplot(gs[0:4, 0:4])
+#
+#     # have to plot the trajectories themselves:grab them!
+#     trajgroup = groups[i]
+#
+#     # define map boundaries based on country code in EASY ACCES excel sheet
+#     mapcorners = [0, -70, 360, -20]
+#     maxlat = mapcorners[3]
+#     minlat = mapcorners[1]
+#     maxlon = mapcorners[2]
+#     minlon = mapcorners[0]
+#
+#     # build the map
+#     map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=minlon, urcrnrlon=maxlon, resolution='l')
+#     map.drawmapboundary(fill_color='lightgrey')
+#     map.fillcontinents(color='darkgrey')
+#     map.drawcoastlines(linewidth=0.1)
+#
+#     # add the trajectories
+#     color_dict = {10: 'blue', 100: 'black', 200: 'green'}
+#     for traj in trajgroup:
+#         altitude0 = traj.data.geometry.apply(lambda p: p.z)[0]
+#         traj.trajcolor = color_dict[altitude0]
+#     for traj in trajgroup:
+#         map.plot(*traj.path.xy, c=traj.trajcolor, latlon=True, zorder=20, alpha=0.1)
+# #
+#     # add the MEANS
+#     # y, x = map(this_site_traj_means['y'], this_site_traj_means['x'])
+#     # map.scatter(x, y, marker='o', edgecolor='black', facecolors='black', alpha=0.5, label=f'{codenames[i]}')
+#     # plt.legend()
+#     map.drawparallels(np.arange(-90, 90, 10), labels=[True, False, False, False], linewidth=0.5)
+#     map.drawmeridians(np.arange(-180, 180, 10), labels=[1, 1, 0, 1], linewidth=0.5)
+#
+#     xtr_subsplot = fig.add_subplot(gs[4:6, 0:4])
+#     plt.show()
+
+
+
+
+
+
+
+
+
+# size1 = 10
+# # INITIALIZE FIGURE
+# fig = plt.figure(figsize=(12,8))
+# gs = gridspec.GridSpec(6, 4)
+# gs.update(wspace=.25, hspace=0.15)
+#
+# # INITIALIZE FIRST SUBPLOT (RAUL MARIN)
+# xtr_subsplot = fig.add_subplot(gs[0:2, 0:2])
+# trajgroup = pysplit.make_trajectorygroup(r'C:/trajectories/colgate/raul_marin/*')
+# mapcorners = [-55-80, -70, -55, -30]
+# standard_pm = None
+# maxlat = mapcorners[3]
+# minlat = mapcorners[1]
+# nz_max_lon = mapcorners[2]
+# nz_min_lon = mapcorners[0]
+# map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=nz_min_lon, urcrnrlon=nz_max_lon, resolution='l')
+# map.drawmapboundary(fill_color='lightgrey')
+# map.fillcontinents(color='darkgrey')
+# map.drawcoastlines(linewidth=0.1)
+# color_dict = {10 : 'blue'}
+# for traj in trajgroup:
+#     altitude0 = traj.data.geometry.apply(lambda p: p.z)[0]
+#     traj.trajcolor = color_dict[altitude0]
+# for traj in trajgroup:
+#     map.plot(*traj.path.xy, c=traj.trajcolor, latlon=True, zorder=20, alpha=0.1)
+# y, x = map(rm['y'], rm['x'])
+# map.scatter(x, y, marker='o', edgecolor='black', facecolors='black', alpha=0.5, s=size1, label='Raul Marin')
+# plt.legend()
+# map.drawparallels(np.arange(-90, 90, 10), labels=[True, False, False, False], linewidth=0.5)
+# # map.drawmeridians(np.arange(-180, 180, 10), labels=[1, 1, 0, 1], linewidth=0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# # We're still going to loop through each site using the codenames listed in the previous scripts as well
+# easy_access = pd.read_excel(r'C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/easy_access2.xlsx')
+#
+# # Read in the sheet that was made from the prvious script (hysplit_prepare_output.py)
+# means_dataframe = pd.read_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/means_dataframe.xlsx')
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# # # there's an issue with Raul Marin trajectories I need to tackle...I'll remove it for now...
+# # easy_access = easy_access.loc[easy_access['Codename'] != 'Raul'].reset_index(drop=True)
+# # means_dataframe = means_dataframe.loc[means_dataframe['Codename'] != 'Raul'].reset_index(drop=True)
+#
+# codenames = easy_access['Codename']
+# print(codenames)
+# code = easy_access['Code']
+# #
+# # Read in the sheet that was made from the prvious script (hysplit_prepare_output.py)
+# means_dataframe = pd.read_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/means_dataframe.xlsx')
+#
+# # testing to make sure I have output for every site:
+# print(np.unique(means_dataframe['Codename']))
+#
+# # iterate through the sites based on their codenames:
+# for i in range(0, len(codenames)):
+#     print(codenames[i])
+#     # locate site information based on the codename
+#     easy_access_information = easy_access.loc[easy_access['Codename'] == codenames[i]]
+#     easy_access_country_code = code[i]
+#
+#     this_site_traj_means = means_dataframe.loc[means_dataframe['Codename'] == codenames[i]]
+#     print(this_site_traj_means)
+#
+#     # Initialize the Figure
+#     fig = plt.figure(figsize=(4, 4))
+#     gs = gridspec.GridSpec(6, 4)
+#     gs.update(wspace=.25, hspace=0.15)
+#     standard_pm = None
+#
+#     # Initialize first subplot
 #     xtr_subsplot = fig.add_subplot(gs[0:4, 0:4])
 #
 #     # have to plot the trajectories themselves:grab them!
 #     trajgroup = pysplit.make_trajectorygroup(f'C:/trajectories/iteration2/{codenames[i]}/*')
-#     mapcorners = [-55-80, -70, -55, -30]
 #
-#     a = plt.axes([.65, .6, .2, .2], facecolor='y')
+#     # define map boundaries based on country code in EASY ACCES excel sheet
+#     mapcorners = [0, -70, 360, -20]
+#     maxlat = mapcorners[3]
+#     minlat = mapcorners[1]
+#     maxlon = mapcorners[2]
+#     minlon = mapcorners[0]
 #
+#     # build the map
+#     map = Basemap(llcrnrlat=minlat, urcrnrlat=maxlat, llcrnrlon=minlon, urcrnrlon=maxlon, resolution='l')
+#     map.drawmapboundary(fill_color='lightgrey')
+#     map.fillcontinents(color='darkgrey')
+#     map.drawcoastlines(linewidth=0.1)
 #
+#     # add the trajectories
+#     color_dict = {10: 'blue', 100: 'black', 200: 'green'}
+#     for traj in trajgroup:
+#         altitude0 = traj.data.geometry.apply(lambda p: p.z)[0]
+#         traj.trajcolor = color_dict[altitude0]
+#     for traj in trajgroup:
+#         map.plot(*traj.path.xy, c=traj.trajcolor, latlon=True, zorder=20, alpha=0.1)
+# #
+#     # add the MEANS
+#     y, x = map(this_site_traj_means['y'], this_site_traj_means['x'])
+#     map.scatter(x, y, marker='o', edgecolor='black', facecolors='black', alpha=0.5, label=f'{codenames[i]}')
+#     plt.legend()
+#     map.drawparallels(np.arange(-90, 90, 10), labels=[True, False, False, False], linewidth=0.5)
+#     map.drawmeridians(np.arange(-180, 180, 10), labels=[1, 1, 0, 1], linewidth=0.5)
 #
 #     xtr_subsplot = fig.add_subplot(gs[4:6, 0:4])
-#
-#
-#
-#
-# plt.show()
-#
+# # #
+# # #
 
-
-
+# #
+# #
+# #
+#
 
 
 
@@ -251,10 +463,10 @@ Old version
 # trajgroup3 = pysplit.make_trajectorygroup(r'C:/trajectories/colgate/kapuni/*')
 # trajgroup4 = pysplit.make_trajectorygroup(r'C:/trajectories/colgate/campbell_island/*')
 # #
-mapcorners1 =  [-55-80, -60, -55, -30]
-mapcorners2 =  [-55-80, -60, -55, -30]
-mapcorners3 =  [100, -60, 180, -30]
-mapcorners4 =  [100, -60, 180, -30]
+# mapcorners1 =  [-55-80, -60, -55, -30]
+# mapcorners2 =  [-55-80, -60, -55, -30]
+# mapcorners3 =  [100, -60, 180, -30]
+# mapcorners4 =  [100, -60, 180, -30]
 # standard_pm = None
 #
 # tra = [trajgroup1, trajgroup2, trajgroup3, trajgroup4]
--- hysplit_prepare_output.py
+++ hysplit_prepare_output.py
@@ -13,7 +13,7 @@ from os.path import isfile, join
 Needs updating to do trajectories for everty site. Old version commented out below
 """
 
-df = pd.read_excel(r'C:/easy_access.xlsx')
+df = pd.read_excel(r'C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/easy_access2.xlsx')
 codenames = df['Codename']
 
 # Location of your tdump file
@@ -90,14 +90,13 @@ for j in range(0, len(codenames)):
                 })
         print("{} points found".format(len(points)))
 
-points = pd.DataFrame(points)
-# points.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/points.xlsx')
-#
+points = pd.DataFrame(points).reset_index(drop=True)
+points.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/points.xlsx')
+
 
 means_dataframe = pd.DataFrame()
 for k in range(0, len(codenames)):
     x = points.loc[points['location'] == codenames[k]]
-
 
     mean_x = x.groupby('timestep').mean().reset_index()
     print(mean_x)
@@ -108,104 +107,3 @@ for k in range(0, len(codenames)):
 
 means_dataframe.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/means_dataframe.xlsx')
 
-
-#
-# """
-# Version 1 below
-# """
-#
-# names = ['raul_marin','monte_tarn','kapuni','campbell_island', 'baja_rosales']
-# # Location of your tdump file
-# points = []
-#
-# for j in range(0, len(names)):
-#     tdump_file = f'C:/trajectories/colgate/{names[j]}/'
-#
-#     onlyfiles = [f for f in listdir(tdump_file) if isfile(join(tdump_file, f))]
-#     # Store the points in this object
-#
-#
-#     # Open the file for reading
-#     print("Reading trajectory information")
-#
-#     for i in range(0, len(onlyfiles)):
-#         with open(f'C:/trajectories/colgate/{names[j]}/{onlyfiles[i]}', "r") as handle:
-#             # split the file into lines
-#             lines = handle.read().split("\n")
-#             # Track the number of files
-#             trajectory_file_count = None
-#             # Track the number of starting points
-#             starting_point_count = None
-#             # Flag to skip the output type line
-#             skip_output_type = True
-#             # loop through lines
-#             for line in lines:
-#                 # Split the line based on spaces and remove empty ones
-#                 data = list(filter(lambda x: not x == "", line.split(" ")))
-#
-#                 # Skip blank lines
-#                 if len(data) == 0:
-#                     continue
-#
-#                 # The first line we encounter is the trajectory file count
-#                 if trajectory_file_count is None:
-#                     trajectory_file_count = int(data[0])
-#                     continue
-#                 # Then we skip lines equal to the trajectory file count
-#                 if trajectory_file_count > 0:
-#                     trajectory_file_count -= 1
-#                     continue
-#
-#                 # Repeat for trajectory points
-#                 if starting_point_count is None:
-#                     starting_point_count = int(data[0])
-#                     continue
-#
-#                 if starting_point_count > 0:
-#                     starting_point_count -= 1
-#                     continue
-#
-#                 # Skip the output flag line if not already done
-#                 if skip_output_type:
-#                     skip_output_type = False
-#                     continue
-#                 runtime = 2
-#                 print(names[j])
-#                 # Now we can read points from the trajectory
-#                 points.append({
-#                     "location": names[j],
-#                     "start_point": int(data[0]),
-#                     "grid": int(data[1]),
-#                     "year": int(data[2]),
-#                     "month": int(data[3]),
-#                     "day": int(data[4]),
-#                     "hour": int(data[5]),
-#                     "minute": int(data[6]),
-#                     "forecast": int(data[7]),
-#                     "timestep": float(data[8]),
-#                     "y": float(data[9]),
-#                     "x": float(data[10]),
-#                     "z": float(data[11]),
-#                     "p": float(data[12]),
-#                 })
-#         print("{} points found".format(len(points)))
-#
-# points = pd.DataFrame(points)
-# points.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/points.xlsx')
-# rm = points.loc[points['location'] == 'raul_marin']
-# mt = points.loc[points['location'] == 'monte_tarn']
-# kp = points.loc[points['location'] == 'kapuni']
-# ci = points.loc[points['location'] == 'campbell_island']
-# bj = points.loc[points['location'] == 'baja_rosales']
-#
-# rm = rm.groupby('timestep').mean().reset_index()
-# mt = mt.groupby('timestep').mean().reset_index()
-# kp = kp.groupby('timestep').mean().reset_index()
-# ci = ci.groupby('timestep').mean().reset_index()
-# bj = bj.groupby('timestep').mean().reset_index()
-#
-# rm.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/rm.xlsx')
-# mt.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/mt.xlsx')
-# kp.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/kp.xlsx')
-# ci.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/ci.xlsx')
-# bj.to_excel('C:/Users/lewis/venv/python310/python-masterclass-remaster-shared/bj.xlsx')
